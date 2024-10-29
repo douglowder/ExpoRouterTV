f73b6870f08add6d7519afe94903b31304722153
Douglas Lowder <douglowder@mac.com>
2024-10-29 15:17:59 -0700

    Plugin for material Android theme

---
+++ with-android-app-theme.js
@@ -0,0 +1,24 @@
+const { withAndroidStyles } = require('expo/config-plugins');
+
+function withCustomAppTheme(config, customAppTheme) {
+  return withAndroidStyles(config, (config) => {
+    let modified = false;
+    const styles = config.modResults;
+    styles?.resources?.style?.map((style) => {
+      if (style.$.name === 'AppTheme') {
+        if (!modified) {
+          style.$.parent = customAppTheme;
+          modified = true;
+        } else {
+          styles?.resources?.style?.splice(
+            styles?.resources?.style?.indexOf(style),
+            1,
+          );
+        }
+      }
+    });
+    return config;
+  });
+}
+
+module.exports = withCustomAppTheme;
