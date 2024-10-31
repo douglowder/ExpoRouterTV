0705ddcae9e5eff072debd52151592a2f1a36c8d
Douglas Lowder <douglowder@mac.com>
2024-10-31 10:26:26 -0700

    React Navigation tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,63 +1,97 @@
+import { Tabs } from 'expo-router';
 import React from 'react';
+import { Platform, Pressable } from 'react-native';
+import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { withLayoutContext } from 'expo-router';
-import { createNativeBottomTabNavigator } from 'react-native-bottom-tabs/react-navigation';
-import { Platform } from 'react-native';
-
-export const Tabs = withLayoutContext(
-  createNativeBottomTabNavigator().Navigator,
-);
+import { useTextStyles } from '@/hooks/useTextStyles';
+import { useScale } from '@/hooks/useScale';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-  const colors = Colors[colorScheme ?? 'light'];
+  const textStyles = useTextStyles();
+  const { scale } = useScale();
+
+  const tabBarButton = (props: BottomTabBarButtonProps) => {
+    const style: any = props.style ?? {};
+    return (
+      <Pressable
+        {...props}
+        style={({ pressed, focused }) => [
+          style,
+          {
+            opacity: pressed || focused ? 0.6 : 1.0,
+          },
+        ]}
+      />
+    );
+  };
+
   return (
     <Tabs
-      tabBarActiveTintColor={colors.tabIconSelected}
-      tabBarInactiveTintColor={colors.tabIconDefault}
-      barTintColor={colors.background}
-      rippleColor={colors.tint}
-      labeled={true}
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarStyle: {
+          height: textStyles.title.lineHeight * 2.5,
+          marginTop: -20 * scale,
+          paddingBottom: textStyles.title.lineHeight * 0.5,
+        },
+        tabBarPosition: Platform.OS === 'ios' ? 'top' : 'bottom',
+        headerShown: false,
+      }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/home.png')
-              : null,
+          tabBarButton,
+          tabBarLabelStyle: textStyles.default,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
           title: 'Explore',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/code.png')
-              : null,
+          tabBarButton,
+          tabBarLabelStyle: textStyles.default,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'code-slash' : 'code-slash-outline'}
+              color={color}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="tv_focus"
         options={{
           title: 'TV event demo',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/tv.png')
-              : null,
+          tabBarButton,
+          tabBarLabelStyle: textStyles.default,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'tv' : 'tv-outline'} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="video"
         options={{
           title: 'Video demo',
-          tabBarIcon: () =>
-            Platform.OS === 'android'
-              ? require('@/assets/bar_icons/video.png')
-              : null,
+          tabBarButton,
+          tabBarLabelStyle: textStyles.default,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'videocam' : 'videocam-outline'}
+              color={color}
+            />
+          ),
         }}
       />
     </Tabs>
